3eaf945893f3586b17f6bca8335af97741bfa8ea
kanica-goddard <kanicagoddard@gmail.com>
2024-01-28 16:06:13 -0500

    manipulate slices

--- main.go
+++ main.go
@@ -9,15 +9,22 @@ func main() {
 
 	var purchaseIDs []int
 
-	purchaseIDs = append(purchaseIDs, 12, 11, 2, 11, 112)
-	sort.Ints(purchaseIDs)
+	purchaseIDs = append(purchaseIDs, 11, 122, 2, 99, 53)
+	fmt.Println("Original list: ", purchaseIDs)
 
-	fmt.Println(purchaseIDs)
+	sort.Ints(purchaseIDs) // sort values in an ascending manner
 
-	sort.Slice(purchaseIDs, func(i, j int) bool {
-		return purchaseIDs[i] < purchaseIDs[j]
+	fmt.Println("Ascending Order: ", purchaseIDs)
+
+	sort.Slice(purchaseIDs, func(i, j int) bool { // sort values in a descending order
+		return purchaseIDs[i] > purchaseIDs[j] // checks for each value based on criteria
 	})
 
-	fmt.Println(purchaseIDs)
+	fmt.Println("Descending Order", purchaseIDs)
+
+	fmt.Println("Value at Index 4: ", purchaseIDs[2])
+	value_removed := purchaseIDs[2]
+	purchaseIDs = append(purchaseIDs[:2], purchaseIDs[2+1:]...) // remove value at the nth index
+	fmt.Printf("removing: %d "+" new values: %d\n", value_removed, purchaseIDs)
 
 }
